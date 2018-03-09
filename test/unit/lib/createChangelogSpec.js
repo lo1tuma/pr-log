572c1b11aa7bee260f5659a54f03baef98bbc7bd
Mathias Schreck <schreck.mathias@googlemail.com>
2018-03-09 23:26:35 +0100

    Use same order for changelog sections as in validLabels

--- test/unit/lib/createChangelogSpec.js
+++ test/unit/lib/createChangelogSpec.js
@@ -58,10 +58,10 @@ test('creates a formatted changelog', (t) => {
 
 test('uses custom labels when provided', (t) => {
     const createChangelog = createChangelogFactory({ getCurrentDate: () => new Date(0), packageInfo: {} });
-    const customValidLabels = {
-        core: 'Core Features',
-        addons: 'Addons'
-    };
+    const customValidLabels = [
+        [ 'core', 'Core Features' ],
+        [ 'addons', 'Addons' ]
+    ];
     const mergedPullRequests = [
         {
             id: '1',
@@ -88,6 +88,47 @@ test('uses custom labels when provided', (t) => {
         '',
         '### Addons',
         '',
+        '* Fixed bug bar ([#2](https://github.com/any/repo/pull/2))',
+        ''
+    ].join('\n');
+
+    const changelog = createChangelog('1.0.0', customValidLabels, mergedPullRequests, 'any/repo');
+
+    t.true(changelog.includes(expectedChangelog));
+});
+
+test('uses the same order for the changelog sections as in validLabels', (t) => {
+    const createChangelog = createChangelogFactory({ getCurrentDate: () => new Date(0), packageInfo: {} });
+    const customValidLabels = [
+        [ 'first', 'First Section' ],
+        [ 'second', 'Second Section' ]
+    ];
+    const mergedPullRequests = [
+        {
+            id: '1',
+            title: 'Fixed bug foo',
+            label: 'second'
+        },
+        {
+            id: '2',
+            title: 'Fixed bug bar',
+            label: 'second'
+        },
+        {
+            id: '3',
+            title: 'Fix spelling error',
+            label: 'first'
+        }
+    ];
+
+    const expectedChangelog = [
+        '### First Section',
+        '',
+        '* Fix spelling error ([#3](https://github.com/any/repo/pull/3))',
+        '',
+        '### Second Section',
+        '',
+        '* Fixed bug foo ([#1](https://github.com/any/repo/pull/1))',
         '* Fixed bug bar ([#2](https://github.com/any/repo/pull/2))',
         ''
     ].join('\n');
