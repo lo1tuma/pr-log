d99283b45943a4f4400ec1f59795223ab17d4c9d
Michael Shilman <michael@lab80.co>
2018-11-06 11:21:34 +0800

    Generate changelog with cherry-picked merges

--- bin/pr-log.js
+++ bin/pr-log.js
@@ -7,10 +7,9 @@ import cli from '../cli';
 program
     .version(config.version)
     .option('--sloppy', 'Skip ensuring clean local git state.')
+    .option('--cherry-pick', 'Use cherry-picks instead of PR merges.')
     .usage('<version-number>')
     .parse(process.argv);
 
-const options = { sloppy: program.sloppy };
-cli
-    .run(program.args[0], options)
-    .done();
+const options = { sloppy: program.sloppy, cherryPick: program.cherryPick };
+cli.run(program.args[0], options).done();
