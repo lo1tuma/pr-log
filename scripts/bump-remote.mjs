2ae0c7c04ec243b49e0b701b32eedcda3c99a072
ncpa0cpl <szymonb21@gmail.com>
2023-04-25 21:11:54 +0200

    ci: added a new workflow for publishing new releases

---
+++ scripts/bump-remote.mjs
@@ -0,0 +1,63 @@
+import axios from "axios";
+import crypto from "crypto";
+
+const getRandomString = (length) => {
+  return crypto.randomBytes(Math.ceil(length / 2)).toString("hex");
+};
+
+const getEnv = (key) => {
+  const variable = process.env[key];
+
+  if (!variable) {
+    throw new Error(`Env var ${key} is not set.`);
+  }
+
+  return variable;
+};
+
+export function encrypt(text) {
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv(
+    "aes-256-cbc",
+    getEnv("GH_BOT_ENCRYPTION_SECRET"),
+    iv
+  );
+  let crypted = cipher.update(text, "utf8", "hex");
+  crypted += cipher.final("hex");
+  return {
+    iv: iv.toString("hex"),
+    content: crypted,
+  };
+}
+
+async function main() {
+  try {
+    const version = process.argv[2];
+    const accessToken = getEnv("GH_BOT_AUTH_TOKEN");
+    const apiUrl = getEnv("GH_BOT_API_URL");
+
+    if (!version) {
+      throw new Error("Version argument must be specified");
+    }
+
+    const data = encrypt(
+      JSON.stringify({
+        authToken: accessToken,
+        repositoryOwner: "ncpa0",
+        repositoryName: "pr-changelog-gen",
+        branch: "master",
+        version,
+        irrelevantGibberish: getRandomString(Math.round(Math.random() * 28) + 4),
+      })
+    );
+
+    await axios.post(`/bump-version`, data, {
+      baseURL: apiUrl,
+    });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+main();
---
+++ scripts/publish.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -o verbose
+set -o xtrace
+set -o errexit
+
+issemver=$(./scripts/check-semver.sh -t "$TAG_NAME")
+currenttag=$(npm pkg get version)
+
+git config user.name github-actions
+git config user.email github-actions@github.com
+
+if [ "$issemver" -eq "1" ]; then
+    if ! [ "$currenttag" = "\"$TAG_NAME\"" ]; then
+        echo "Branch tag is different than packge.json version. Updating package.json version to $TAG_NAME"
+        npm version "$TAG_NAME"
+        echo "Requesting GithubBot to bump the package.json version on the remote"
+        node ./scripts/bump-remote.mjs "$TAG_NAME"
+    fi
+    echo "Publishing to npm"
+    npm publish --access public
+fi
